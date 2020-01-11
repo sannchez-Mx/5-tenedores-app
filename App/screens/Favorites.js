5e51af6176d267dce0db58e63ecbad109e594642
Roberto Sanchez <sannchez-mx@outlook.com>
2020-01-10 23:57:15 -0600

    Third commit

--- App/screens/Favorites.js
+++ App/screens/Favorites.js
@@ -19,7 +19,8 @@ import Loading from "../components/Loading";
 import {
   firestoreDB,
   firebaseUserUid,
-  firebaseDownloadImages
+  firebaseDownloadImages,
+  firebaseAuthState
 } from "../utils/FireBase";
 
 export default function Favorites(props) {
@@ -28,41 +29,38 @@ export default function Favorites(props) {
   const [restaurants, setRestaurants] = useState([]);
   const [reloadRestaurants, setReloadRestaurants] = useState(false);
   const [isVisibleLoding, setIsVisibleLoading] = useState(false);
-  const [userLogged, setUserLogged] = useState(true);
+  const [userLogged, setUserLogged] = useState(false);
 
   const toastRef = useRef();
 
-  useEffect(
-    () => {
-      if (userLogged) {
-        const id_user = firebaseUserUid();
-        firestoreDB
-          .collection("favorites")
-          .where("id_user", "==", id_user)
-          .get()
-          .then(response => {
-            const idRestaurantsArray = [];
-            response.forEach(doc => {
-              idRestaurantsArray.push(doc.data().id_restaurant);
-            });
+  firebaseAuthState(setUserLogged);
 
-            getDataRestaurants(idRestaurantsArray).then(response => {
-              const restaurants = [];
-              response.forEach(doc => {
-                let restaurant = doc.data();
-                restaurant.id = doc.id;
-                restaurants.push(restaurant);
-              });
-              setRestaurants(restaurants);
-            });
+  useEffect(() => {
+    if (userLogged) {
+      const id_user = firebaseUserUid();
+      firestoreDB
+        .collection("favorites")
+        .where("id_user", "==", id_user)
+        .get()
+        .then(response => {
+          const idRestaurantsArray = [];
+          response.forEach(doc => {
+            idRestaurantsArray.push(doc.data().id_restaurant);
           });
-      }
-      //setReloadRestaurants(false);
-    },
-    [
-      //  reloadRestaurants
-    ]
-  );
+
+          getDataRestaurants(idRestaurantsArray).then(response => {
+            const restaurants = [];
+            response.forEach(doc => {
+              let restaurant = doc.data();
+              restaurant.id = doc.id;
+              restaurants.push(restaurant);
+            });
+            setRestaurants(restaurants);
+          });
+        });
+    }
+    setReloadRestaurants(false);
+  }, [reloadRestaurants]);
 
   const getDataRestaurants = idRestaurantsArray => {
     const arrayRestaurants = [];
@@ -76,8 +74,22 @@ export default function Favorites(props) {
     return Promise.all(arrayRestaurants);
   };
 
+  if (!userLogged) {
+    return (
+      <UserNoLogged
+        setReloadRestaurants={setReloadRestaurants}
+        navigation={navigation}
+      />
+    );
+  }
+
+  if (restaurants.length === 0) {
+    return <NotFoundRestaurants setReloadRestaurants={setReloadRestaurants} />;
+  }
+
   return (
     <View style={styles.viewBody}>
+      <NavigationEvents onWillFocus={() => setReloadRestaurants(true)} />
       {restaurants ? (
         <FlatList
           data={restaurants}
@@ -98,6 +110,8 @@ export default function Favorites(props) {
           <Text>Cargando restaurantes</Text>
         </View>
       )}
+      <Toast ref={toastRef} position="center" opacity={1} />
+      <Loading text="Eliminando Restaurante" isVisible={isVisibleLoding} />
     </View>
   );
 }
@@ -124,6 +138,53 @@ function Restaurant(props) {
     }
   }, []);
 
+  const confirmRemoveFavorite = () => {
+    Alert.alert(
+      "Eliminar Restaurante de Favoritos",
+      "¿Estas seguro de que quieres eliminar el restaurante de favoritos?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "Eliminar",
+          onPress: removeFavorite
+        }
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const removeFavorite = () => {
+    setIsVisibleLoading(true);
+    const id_user = firebaseUserUid();
+    firestoreDB
+      .collection("favorites")
+      .where("id_restaurant", "==", id)
+      .where("id_user", "==", id_user)
+      .get()
+      .then(response => {
+        response.forEach(doc => {
+          const idFavorite = doc.id;
+          firestoreDB
+            .collection("favorites")
+            .doc(idFavorite)
+            .delete()
+            .then(() => {
+              setIsVisibleLoading(false);
+              setReloadRestaurants(true);
+              toastRef.current.show("Restaurante eliminado correctamente");
+            })
+            .catch(() => {
+              toastRef.current.show(
+                "Error al eliminar el restaurante, intentelo más tarde"
+              );
+            });
+        });
+      });
+  };
+
   return (
     <View style={styles.restaurant}>
       <TouchableOpacity
@@ -145,11 +206,45 @@ function Restaurant(props) {
           name="heart"
           color="#00a680"
           containerStyle={styles.favorite}
-          //onPress={confirmRemoveFavorite}
+          onPress={confirmRemoveFavorite}
           size={40}
           underlayColor="transparent"
         />
       </View>
+    </View>
+  );
+}
+
+function NotFoundRestaurants(props) {
+  const { setReloadRestaurants } = props;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <NavigationEvents onWillFocus={() => setReloadRestaurants(true)} />
+      <Icon type="material-community" name="alert-outline" size={50} />
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        No tienes restaurantes en tu lista
+      </Text>
+    </View>
+  );
+}
+
+function UserNoLogged(props) {
+  const { setReloadRestaurants, navigation } = props;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <NavigationEvents onWillFocus={() => setReloadRestaurants(true)} />
+      <Icon type="material-community" name="alert-outline" size={50} />
+      <Text style={{ fontSize: 20, fontWeight: "bold", textAlign: "center" }}>
+        Necesitas estar logeado para ver esta sección.
+      </Text>
+      <Button
+        title="Ir al login"
+        onPress={() => navigation.navigate("Login")}
+        containerStyle={{ marginTop: 20, width: "80%" }}
+        buttonStyle={{ backgroundColor: "#00a680" }}
+      />
     </View>
   );
 }
